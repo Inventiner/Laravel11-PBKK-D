7992839df61f1a2c8979d1f3631379559a5bc79d
Inventiner <46766058+Inventiner@users.noreply.github.com>
2024-09-12 00:58:56 +0700

    Initial Commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+        <title>PBKK D Laravel Project</title>
+
+    </head>
+    <body class="h-full">
+        <div class="min-h-full">
+            <x-navbar></x-navbar>
+            <x-header>{{ $title }}</x-header>
+            <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {{ $slot }}
+            </div>
+            </main>
+        </div>  
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    
+    <p class="font-semibold text-gray-1000 text-base md:text-lg leading-none mb-2"> Hi This is a website for my Laravel Project, Expect more features to be added overtime </p>
+
+    - Currently being updated is the About Me page
+</x-layout>
